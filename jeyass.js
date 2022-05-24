ecff2cba205d952e1fd29b47d8116d79f5a18987
Harrys Potter <blackiaratanmusic@gmail.com>
2022-05-24 15:26:00 -0300

    Js add

--- jeyass.js
+++ jeyass.js
@@ -31,3 +31,16 @@ for(let i=0; i<toggles.length; i++){
         }
     })
 }
+
+const hamburguerr = document.querySelector(".hamburguerr");
+const list = document.querySelector(".list");
+
+hamburguerr.addEventListener("click", ()=> {
+    hamburguerr.classList.toggle("active");
+    list.classList.toggle("active");
+})
+
+document.querySelectorAll(".list").forEach(n => n.addEventListener("click", () =>{
+    hamburguerr.classList.remove("active");
+    list.classList.remove("active");
+}))
